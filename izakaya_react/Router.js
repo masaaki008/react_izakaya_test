521caa7ab10a72b308e03ea87d8dfdfa6d79edc5
masaaki008 <uver.railgun.next@gmail.com>
2018-11-18 23:43:07 +0900

    add new files

--- izakaya_react/Router.js
+++ izakaya_react/Router.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Scene, Router, Tabs } from 'react-native-router-flux';
 import Home from './screens/Home';
-import News from './screens/News';
 import Icon from 'react-native-vector-icons/MaterialIcons';
  
 const styles = {
@@ -37,18 +36,10 @@ const RouterComponent = () => {
       >
         <Scene
           key='home'
-          component={Article}
+          component={Home}
           title='ホーム'
           iconName='collections'
           initial
-          iconColor='red'
-          icon={TabBarIcon}
-        />
-        <Scene
-          key='news'
-          component={News}
-          title='お知らせ'
-          iconName='textsms'
           iconColor='red'
           icon={TabBarIcon}
         />
